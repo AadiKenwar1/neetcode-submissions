d1806422bb7c971a084c3332c15e65dc4378b508
AadiKenwar1 <aadikenwar983@gmail.com>
2026-08-21 20:48:22 -0400

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        
+        result = 0
+
+        leftStack = []
+        rightStack = []
+        leftMax = 0
+        rightMax = 0
+
+        for i in range(len(height)):
+            #Pointers
+            left = i
+            right = len(height) - 1 - i
+            #Monotonic Stack Creation
+            leftStack.append(leftMax)
+            rightStack.append(rightMax)
+            #Checking New Maxes
+            if(height[i] > leftMax):
+                leftMax = height[left]
+            if(height[len(height) - 1 - i] > rightMax):
+                rightMax = height[right]
+
+
+        rightStack.reverse()
+        for i in range(len(height)):
+            space = max(min(leftStack[i], rightStack[i]) - height[i], 0)
+            result += space
+
+        return result
+            
+
+
